134ea3e0fb7242709c4517ca45400ab97537ce9a
nguyennamcuong <cuong@gmail.com>
2023-01-30 11:56:47 +0700

    Create Table of Products

--- app/Http/Controllers/MenuController.php
+++ app/Http/Controllers/MenuController.php
@@ -15,11 +15,11 @@ class MenuController extends Controller
     }
     public function index(){
         $menus = $this->menu->paginate(5);
-        return view('menus.index',compact('menus'));
+        return view('admin.menus.index',compact('menus'));
     }
     public function create(){
         $optionSelect = $this->menuRecusive->menuRecusiveAdd();
-        return view('menus.add',compact('optionSelect'));
+        return view('admin.menus.add',compact('optionSelect'));
     }
     public function store(Request $request){
         $this->menu->create([
@@ -33,7 +33,7 @@ class MenuController extends Controller
     public function edit($id, Request $request)
     {       $menuFollowEdit = $this->menu->find($id);
             $optionSelect = $this->menuRecusive->menuRecusiveEdit($menuFollowEdit->parent_id);
-            return view('menus.edit',compact('optionSelect','menuFollowEdit'));
+            return view('admin.menus.edit',compact('optionSelect','menuFollowEdit'));
     }
     public function update($id, Request $request){
         $this->menu->find($id)->update(

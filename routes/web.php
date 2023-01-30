c738e35acf0f5cccd1a849bce7194ecf92029278
nguyennamcuong <cuong@gmail.com>
2023-01-30 23:51:40 +0700

    Show Add Product Form

--- routes/web.php
+++ routes/web.php
@@ -85,7 +85,10 @@ Route::prefix('admin')->group(function () {
             'as' =>'product.index',
             'uses'=>'AdminProductController@index'
         ]);
-
+        Route::get('/create', [
+            'as' =>'product.create',
+            'uses'=>'AdminProductController@create'
+        ]);
     });
 
 });
